b2fd45bf14b37385c39ae67461d4db171f4ba128
Muhammad Rezki Ananda <muhammad.ananda@tiket.com>
2022-04-17 08:11:21 +0700

    Get All Data

---
+++ controllers/pegawai.controller.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"github.com/labstack/echo/v4"
+	"myapp/models"
+	"net/http"
+)
+
+func FetchAllPegawai(c echo.Context) error {
+	result, err := models.FetchAllPegawai()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"myapp/controllers"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,6 +12,8 @@ func Init() *echo.Echo {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, this is echo !")
 	})
+
+	e.GET("/pegawai", controllers.FetchAllPegawai)
 
 	return e
 }
